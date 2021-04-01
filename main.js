247c33639c4f5c2d6ed966353a96f366ecb8ea06
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-01 11:03:45 +0100

    Vue mastery intro to vue - 7. computed props

---
+++ main.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        brand: 'Vue Mastery',
+        product: 'Socks',
+        selectedVariant: 0,
+        inventory: 100,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            { variantId: 2234, variantColor: 'green', 
+            variantImage: './assets/vmSocks-green-onWhite.jpg', 
+            variantQuantity: 100 },
+            { variantId: 2235, variantColor: 'blue', 
+            variantImage: './assets/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0 },
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+    },
+})
