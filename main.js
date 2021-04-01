0013bd51ae23be8a88eb4988b7337d6c0dc10f52
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-01 11:48:44 +0100

    Lesson 9 - Communicating Events

--- main.js
+++ main.js
@@ -39,10 +39,6 @@ Vue.component('product', {
 
         <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to Cart</button>
 
-        <div class="cart">
-            <p>Cart({{cart}})</p>
-        </div>
-
     </div>
 
 </div>
@@ -62,12 +58,11 @@ Vue.component('product', {
                 variantImage: './assets/vmSocks-blue-onWhite.jpg',
                 variantQuantity: 0 },
             ],
-            cart: 0
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1;
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
         updateProduct(index) {
             this.selectedVariant = index;
@@ -96,6 +91,12 @@ Vue.component('product', {
 var app = new Vue({
     el: '#app',
     data: {
-        premium: false
+        premium: false,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
     }
 })
